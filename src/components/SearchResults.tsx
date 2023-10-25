2f561e5786fd98895ca97ad8ba79f0afe042a1fc
johngaalt <rajon777666@gmail.com>
2023-10-25 14:45:08 +0300

    feat: show data on the page

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -4,10 +4,12 @@ import { SearchResultsProps } from "./SearchResults.types";
 export default class SearchResults extends React.Component<SearchResultsProps> {
   render(): React.ReactNode {
     const { results } = this.props;
-    console.log(results);
     return (
       <div className="search-results">
         <h1>Search Results</h1>
+        {results.map((result) => (
+          <div key={result.name}>{result.name}</div>
+        ))}
       </div>
     );
   }
