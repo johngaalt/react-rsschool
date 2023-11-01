a36cad0158db7751689714f597e90f7faefedcd1
johngaalt <rajon777666@gmail.com>
2023-11-01 17:15:27 +0300

    refactor: search bar, search results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,58 +1,49 @@
-import React from "react";
-import { SearchBarProps, SearchBarState } from "./SearchBar.types";
+import React, { useEffect, useState } from "react";
+import { SearchBarProps } from "./SearchBar.types";
 
-export default class SearchBar extends React.Component<
-  SearchBarProps,
-  SearchBarState
-> {
-  constructor(props: SearchBarProps) {
-    super(props);
+export default function SearchBar({
+  onSearch,
+}: SearchBarProps): React.JSX.Element {
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-    this.state = {
-      searchTerm: "",
-    };
-  }
+  useEffect(() => {
+    const savedSearchTerm = localStorage.getItem("searchTerm");
+    if (savedSearchTerm) {
+      setSearchTerm(savedSearchTerm);
+    }
+  }, []);
 
-  componentDidMount(): void {
-    const searchTerm = localStorage.getItem("searchTerm");
-    this.setState({ searchTerm });
-  }
-
-  search = (): void => {
-    const { onSearch } = this.props;
-
+  const handleSearch = () => {
     onSearch();
   };
 
-  saveSearchData = (searchTerm: string): void => {
-    localStorage.setItem("searchTerm", searchTerm);
-    this.setState({ searchTerm });
+  const saveSearchData = (term: string): void => {
+    localStorage.setItem("searchTerm", term);
+
+    setSearchTerm(term);
   };
 
-  render(): React.ReactNode {
-    const { searchTerm } = this.state;
-    return (
-      <div className="flex justify-center items-center flex-col mb-5">
-        <h1 className="mr-3  text-3xl text-bold mb-3">
-          Find a person from Star Wars
-        </h1>
-        <div className="flex flex-row">
-          <input
-            className="search-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Search"
-            type="search"
-            onChange={(e) => this.saveSearchData(e.target.value)}
-            value={searchTerm || ""}
-          />
-          <button
-            className="bg-blue-800 text-white rounded py-1 px-4 flex items-center justify-center "
-            type="submit"
-            onClick={this.search}
-          >
-            Search
-          </button>
-        </div>
+  return (
+    <div className="flex justify-center items-center flex-col mb-5">
+      <h1 className="mr-3  text-3xl text-bold mb-3">
+        Find a person from Star Wars
+      </h1>
+      <div className="flex flex-row">
+        <input
+          className="search-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Search"
+          type="search"
+          onChange={(e) => saveSearchData(e.target.value)}
+          value={searchTerm || ""}
+        />
+        <button
+          className="bg-blue-800 text-white rounded py-1 px-4 flex items-center justify-center "
+          type="submit"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,49 +1,45 @@
 import React from "react";
 import { SearchResultsProps } from "./SearchResults.types";
 
-export default class SearchResults extends React.Component<SearchResultsProps> {
-  render(): React.ReactNode {
-    const {
-      results,
-      onNextPage,
-      onPreviousPage,
-      hasNextPage,
-      hasPreviousPage,
-    } = this.props;
+export default function SearchResults({
+  hasNextPage,
+  hasPreviousPage,
+  results,
+  onNextPage,
+  onPreviousPage,
+}: SearchResultsProps): React.ReactElement {
+  const buttonNextClass = hasNextPage ? "bg-blue-800" : "bg-gray-300";
 
-    const buttonNextClass = hasNextPage ? "bg-blue-800" : "bg-gray-300";
+  const buttonPreviousClass = hasPreviousPage ? "bg-blue-800" : "bg-gray-300";
 
-    const buttonPreviousClass = hasPreviousPage ? "bg-blue-800" : "bg-gray-300";
-
-    return (
-      <div className="flex flex-col justify-center items-start ">
-        <div className="flex flex-row mb-5">
-          <h1 className="text-3xl font-bold mr-3 ">Search Results</h1>
-          <button
-            disabled={!hasPreviousPage}
-            className={`flex justify-center items-center py-1 px-2 mr-3  ${buttonPreviousClass} text-white rounded`}
-            type="button"
-            onClick={onPreviousPage}
-          >
-            Previous Page
-          </button>
-          <button
-            disabled={!hasNextPage}
-            className={`flex justify-center items-center py-1 px-2 ${buttonNextClass} text-white rounded`}
-            type="button"
-            onClick={onNextPage}
-          >
-            Next Page
-          </button>
-        </div>
-        {results.map((result) => (
-          <div className="flex mb-4 justify-between gap-3" key={result.name}>
-            <h4 className="text-xl text-left font-bold">{result.name}</h4>
-            <div>birth year: {result.birth_year}</div>
-            <div>eye color: {result.eye_color}</div>
-          </div>
-        ))}
+  return (
+    <div className="flex flex-col justify-center items-start ">
+      <div className="flex flex-row mb-5">
+        <h1 className="text-3xl font-bold mr-3 ">Search Results</h1>
+        <button
+          disabled={!hasPreviousPage}
+          className={`flex justify-center items-center py-1 px-2 mr-3  ${buttonPreviousClass} text-white rounded`}
+          type="button"
+          onClick={onPreviousPage}
+        >
+          Previous Page
+        </button>
+        <button
+          disabled={!hasNextPage}
+          className={`flex justify-center items-center py-1 px-2 ${buttonNextClass} text-white rounded`}
+          type="button"
+          onClick={onNextPage}
+        >
+          Next Page
+        </button>
       </div>
-    );
-  }
+      {results.map((result) => (
+        <div className="flex mb-4 justify-between gap-3" key={result.name}>
+          <h4 className="text-xl text-left font-bold">{result.name}</h4>
+          <div>birth year: {result.birth_year}</div>
+          <div>eye color: {result.eye_color}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
