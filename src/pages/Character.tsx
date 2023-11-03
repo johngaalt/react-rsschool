9b677af1ba5541a1e5756fdaa4154afac0dfd363
johngaalt <rajon777666@gmail.com>
2023-11-03 13:44:28 +0300

    feat: close character page

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import StarWarsService from "../services/StarWarsService";
 import { Character as CharacterType } from "../services/StarWarsService.types";
+import cross from "../assets/cross.svg";
 
 export default function Character() {
   const { id } = useParams();
   const [character, setCharacter] = useState<CharacterType>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const closeRightSection = () => {
+    navigate("/");
+  };
 
   useEffect(() => {
     async function fetchCharacter() {
@@ -22,10 +28,23 @@ export default function Character() {
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-row justify-between items-center gap-10">
         <h1 className="text-orange-500 text-bold text-center text-4xl">
           Star Wars Universe
         </h1>
+        <button
+          onClick={closeRightSection}
+          className="cursor-pointer "
+          type="button"
+        >
+          <img
+            src={cross}
+            alt="cross"
+            width={30}
+            height={30}
+            className="text-orange-500"
+          />
+        </button>
       </div>
       {isLoading ? (
         <div className="flex justify-center items-center">
