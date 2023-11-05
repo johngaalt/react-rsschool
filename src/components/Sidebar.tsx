fa0cff52ab38493f05dfa627a6569e75d15a4a9d
johngaalt <rajon777666@gmail.com>
2023-11-05 14:12:35 +0300

    feat: navigate to home

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
-import { Outlet, useSearchParams } from "react-router-dom";
+import {
+  SyntheticEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
 import StarWarsService from "../services/StarWarsService";
 import { Details } from "../services/StarWarsService.types";
 import ErrorButton from "./ErrorButton";
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
+import { Paths } from "./Router.types";
 
 export default function Sidebar() {
   const [people, setPeople] = useState<Details[]>([]);
@@ -12,6 +19,8 @@ export default function Sidebar() {
   const [hasPreviousPage, setHasPreviousPage] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useSearchParams({ page: "1" });
+  const navigate = useNavigate();
+  const sidebarRef = useRef(null);
 
   const currentPage = Number(searchParams.get("page"));
 
@@ -39,9 +48,21 @@ export default function Sidebar() {
     fetchPeople(currentPage);
   }, [fetchPeople, currentPage]);
 
+  const navigateToHome = (e: SyntheticEvent) => {
+    if (e.target === sidebarRef.current) {
+      navigate(Paths.Home);
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-col w-1/3">
+      <div
+        className="flex flex-col w-1/3 h-full cursor-pointer"
+        ref={sidebarRef}
+        onClick={(e) => {
+          navigateToHome(e);
+        }}
+      >
         <ErrorButton />
         <SearchBar onSearch={fetchPeople} />
         {isLoading ? (
