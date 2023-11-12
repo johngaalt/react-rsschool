035c17472b2248b5c028c1dba4a27487e51c463f
johngaalt <rajon777666@gmail.com>
2023-11-12 16:41:46 +0300

    test: should render details after clicking on search result item

---
+++ src/components/Router.test.tsx
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { wrapWithSidebarContext } from "../test-helpers/SidebarContext";
+import { SearchTermProvider } from "./SearchTermContext";
+import Router from "./Router";
+import userEvent from "@testing-library/user-event";
+
+function renderRouter() {
+  const withSidebarContext = wrapWithSidebarContext(<Router />);
+  return render(
+    <MemoryRouter>
+      <SearchTermProvider>{withSidebarContext}</SearchTermProvider>
+    </MemoryRouter>,
+  );
+}
+
+describe("Router", () => {
+  it.skip("should update url when page is changed to next page", async () => {
+    const user = userEvent.setup();
+    const initialUrl = global.window.location.href;
+    renderRouter();
+
+    const nextButton = await screen.findByText(/next page/i);
+    expect(nextButton).toBeInTheDocument();
+
+    user.click(nextButton);
+
+    const page2 = await screen.findByText(/page: 2/i);
+    expect(page2).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(global.window.location.href).not.toBe(initialUrl);
+    });
+  });
+
+  it("should render details after clicking on search result item", async () => {
+    renderRouter();
+
+    const searchResultItem = await screen.findByText(/character 2/i);
+    fireEvent.click(searchResultItem);
+
+    const detailsHeader = await screen.findByText(/star wars universe/i);
+    expect(detailsHeader).toBeInTheDocument();
+  });
+});
