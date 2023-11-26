129a79b4ba1ac886e25b65fb2de2de73d1f764f7
johngaalt <rajon777666@gmail.com>
2023-11-26 17:51:24 +0300

    test: details

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import Image from "next/image";
 import {
   getAll,
   getById,
@@ -7,8 +6,9 @@ import {
   useGetByIdQuery,
 } from "../../src/state/query";
 import { wrapper } from "src/state/store";
+import Details from "src/components/Details";
 
-export default function Details() {
+export default function DetailsPage() {
   const router = useRouter();
   const { id } = router.query as { id: string };
 
@@ -21,38 +21,11 @@ export default function Details() {
   };
 
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex flex-row justify-between items-center gap-10">
-        <h1 className="text-orange-500 text-bold text-center text-4xl">
-          Star Wars Universe
-        </h1>
-        <button
-          onClick={closeDetailsSection}
-          className="cursor-pointer "
-          type="button"
-        >
-          <Image
-            src="/cross-1.svg"
-            alt="cross"
-            width={30}
-            height={30}
-            className="text-orange-500 hover:scale-125 transition duration-300 ease-in-out"
-          />
-        </button>
-      </div>
-      {isFetching ? (
-        <div className="flex justify-center items-center">
-          <span className="visually-hidden animate-pulse">Loading...</span>
-        </div>
-      ) : (
-        <div className="flex flex-col justify-between items-center border border-orange-500 gap-5 p-5 rounded-lg ">
-          <h2 className="text-3xl font-bold"> {details?.name}</h2>
-          <p className="text-2xl">birth year: {details?.birth_year}</p>
-          <p className="text-2xl">eye color: {details?.eye_color}</p>
-          <p className="text-2xl">hair color: {details?.hair_color}</p>
-        </div>
-      )}
-    </div>
+    <Details
+      details={details}
+      closeDetailsSection={closeDetailsSection}
+      isFetching={isFetching}
+    />
   );
 }
 
