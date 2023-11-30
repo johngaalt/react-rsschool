7d4fc3953c05605691fdf089ba0f6c3d551aed94
johngaalt <rajon777666@gmail.com>
2023-11-30 23:08:49 +0300

    feat: styles input

--- src/components/UncontrolledForm.tsx
+++ src/components/UncontrolledForm.tsx
@@ -14,9 +14,19 @@ export default function UncontrolledForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" ref={inputRef} />
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit} className="flex">
+      <input
+        type="text"
+        ref={inputRef}
+        className="block w-full px-4 py-2 bg-gray-500 text-white border border-white rounded-md shadow-sm placeholder-white focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500"
+        placeholder="Enter text"
+      />
+      <button
+        type="submit"
+        className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-md hover:bg-gray-600 focus:outline-none focus:border-gray-700 focus:ring-2 focus:ring-gray-400"
+      >
+        Submit
+      </button>
     </form>
   );
 }
