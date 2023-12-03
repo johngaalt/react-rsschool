0be4c60e0846da03bfe22a756bb5983306bcfcf9
johngaalt <rajon777666@gmail.com>
2023-12-03 17:10:53 +0300

    refactor: store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formSlice from './formDataSlice';
-import controlledFormSlice from './controlledFormSlice';
 
 export const store = configureStore({
   reducer: {
-    uncontrolledForm: formSlice,
-    controlledForm: controlledFormSlice,
+    formData: formSlice,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppSelector = typeof store.getState;
